2d194e67a176af888ae6f49d724413a4b21ce90a
Rafael <rafekzielinski@wp.pl>
2023-12-14 20:51:37 +0100

    pure structure of project

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/SimpleLoginAndRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLoginAndRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
