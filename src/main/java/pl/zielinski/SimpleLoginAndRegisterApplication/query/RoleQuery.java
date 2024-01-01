62da88885dfbc86b10414994ba3edef9a90f8e8d
Rafael <rafekzielinski@wp.pl>
2024-01-01 18:41:45 +0100

    adding sql query

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
@@ -8,4 +8,5 @@ package pl.zielinski.SimpleLoginAndRegisterApplication.query;
  */
 public class RoleQuery {
     public static final String SELECT_ROLES_QUERY = "SELECT * FROM Roles";
+    public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM Roles WHERE name = :roleName";
 }
