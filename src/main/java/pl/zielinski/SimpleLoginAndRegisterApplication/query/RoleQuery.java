2fa51fe16e1d2ffd5f7de594184828a4fe7d76b9
Rafael <rafekzielinski@wp.pl>
2024-01-01 18:54:38 +0100

    adding Role sql query

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
@@ -9,4 +9,5 @@ package pl.zielinski.SimpleLoginAndRegisterApplication.query;
 public class RoleQuery {
     public static final String SELECT_ROLES_QUERY = "SELECT * FROM Roles";
     public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM Roles WHERE name = :roleName";
+    public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO UserRoles(user_id, role_id) VALUES(:userId, :roleId);";
 }
