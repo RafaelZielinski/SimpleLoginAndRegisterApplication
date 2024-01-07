92e2c13745af94d37fc4741f26704d549fbd4717
Rafael <rafekzielinski@wp.pl>
2024-01-07 19:31:59 +0100

    adding user service implementation

---
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.zielinski.SimpleLoginAndRegisterApplication.domain.User;
+import pl.zielinski.SimpleLoginAndRegisterApplication.dto.UserDTO;
+import pl.zielinski.SimpleLoginAndRegisterApplication.mapper.UserDTOMapper;
+import pl.zielinski.SimpleLoginAndRegisterApplication.repository.UserRepository;
+import pl.zielinski.SimpleLoginAndRegisterApplication.service.UserService;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 07/01/2024
+ */
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository<User> userRepository;
+
+    @Override
+    public UserDTO createUser(User user) {
+        return UserDTOMapper.fromUser(userRepository.create(user));
+    }
+
+    @Override
+    public UserDTO getUserByEmail(String email) {
+        return UserDTOMapper.fromUser(userRepository.getUserByEmail(email));
+    }
+
+    @Override
+    public UserDTO updateUserData(User user) {
+        return UserDTOMapper.fromUser(userRepository.update(user));
+    }
+}
