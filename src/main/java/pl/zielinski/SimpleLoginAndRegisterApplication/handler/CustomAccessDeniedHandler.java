22e88f6de4df4881bf8f189e5b3731fd1071bb4c
Rafael <rafekzielinski@wp.pl>
2024-01-10 19:06:12 +0100

    adding two handlers for security

---
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,43 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import pl.zielinski.SimpleLoginAndRegisterApplication.domain.HttpResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.runtime.ObjectMethods;
+import java.time.LocalDateTime;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 10/01/2024
+ */
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        HttpResponse httpResponse = HttpResponse.builder()
+                .timeStamp(LocalDateTime.now().toString())
+                .reason("You don't have enough permission")
+                .status(HttpStatus.FORBIDDEN)
+                .statusCode(HttpStatus.FORBIDDEN.value())
+                .build();
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        OutputStream out = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(out, httpResponse);
+        out.flush();
+    }
+}
