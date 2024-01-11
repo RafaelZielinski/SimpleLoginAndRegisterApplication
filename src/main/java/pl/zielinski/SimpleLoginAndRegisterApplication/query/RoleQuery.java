57d1a4288a2cd0701746b0769a8d4c853d17e330
Rafael <rafekzielinski@wp.pl>
2024-01-11 06:56:29 +0100

    adding role query searching by user id

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
@@ -11,4 +11,5 @@ public class RoleQuery {
     public static final String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM Roles WHERE name = :roleName;";
     public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO UserRoles(user_id, role_id) VALUES(:userId, :roleId);";
     public static final String SELECT_ROLE_BY_ID = "SELECT * FROM Roles WHERE id = :id;";
+    public static final String SELECT_ROLE_BY_USER_ID = "SELECT * FROM Roles WHERE userId = :id";
 }
