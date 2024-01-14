eeced00d4a1d30448631337d73b92bbbd780983f
Rafael <rafekzielinski@wp.pl>
2024-01-14 21:36:03 +0100

    adding methods in UserDtoMapper

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/mapper/UserDTOMapper.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/mapper/UserDTOMapper.java
@@ -13,10 +13,23 @@ import pl.zielinski.SimpleLoginAndRegisterApplication.dto.UserDTO;
  * @since 07/01/2024
  */
 public class UserDTOMapper {
+
     public static UserDTO fromUser(User user) {
         UserDTO userDTOMapper = new UserDTO();
         BeanUtils.copyProperties(user, userDTOMapper);
         return userDTOMapper;
+    }
+    public static UserDTO fromUser(User user, Role role) {
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        userDTO.setRoleName(role.getName());
+        userDTO.setPermissions(role.getPermission());
+        return userDTO;
+    }
 
+    public static User toUser(UserDTO userDTO) {
+        User user = new User();
+        BeanUtils.copyProperties(userDTO, user);
+        return user;
     }
 }
