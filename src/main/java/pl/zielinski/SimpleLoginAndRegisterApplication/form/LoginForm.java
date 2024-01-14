23b62c567c29fb05bf8be32a103b150359aaa03c
Rafael <rafekzielinski@wp.pl>
2024-01-14 21:32:45 +0100

    adding loginForm

---
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/form/LoginForm.java
@@ -0,0 +1,25 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 14/01/2024
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginForm {
+    @NotEmpty(message = "Email can not be empty")
+    @Email(message = "Invalid email. Please eneter a valid email address")
+    private String email;
+    @NotEmpty(message = "Password can not be empty")
+    private String password;
+}
