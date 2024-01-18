514ea24a1f3d4cb9be413b63e025e9843131dd33
Rafael <rafekzielinski@wp.pl>
2024-01-18 18:45:52 +0100

    adding roleType enum

---
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/enumeration/RoleType.java
@@ -0,0 +1,14 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.enumeration;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 18/01/2024
+ */
+public enum RoleType {
+    ROLE_USER,
+    ROLE_MANAGER,
+    ROLE_ADMIN,
+    ROLE_SYSADMIN
+}
