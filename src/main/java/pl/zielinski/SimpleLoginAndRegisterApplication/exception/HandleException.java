45f3473f0d341010ab14bd7d39e05902a503d11a
Rafael <rafekzielinski@wp.pl>
2024-01-19 22:27:02 +0100

    adding handle methodArgumentNotValid

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/exception/HandleException.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/exception/HandleException.java
@@ -2,14 +2,20 @@ package pl.zielinski.SimpleLoginAndRegisterApplication.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.zielinski.SimpleLoginAndRegisterApplication.domain.HttpResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.time.LocalTime.now;
 
@@ -33,5 +39,16 @@ public class HandleException extends ResponseEntityExceptionHandler implements E
                 .build(), statusCode);
     }
 
-
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(", "));
+        return new ResponseEntity<>(HttpResponse.builder()
+                .timeStamp(now().toString())
+                .reason(fields)
+                .developerMessage(ex.getMessage())
+                .status(HttpStatus.resolve(status.value()))
+                .statusCode(status.value())
+                .build(), status);
+    }
 }
