c541e999894d6cd6868803b73e280f5754bb0f3e
Rafael <rafekzielinski@wp.pl>
2024-01-20 22:02:15 +0100

    adding above 10 handle exceptions

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/exception/HandleException.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/exception/HandleException.java
@@ -1,23 +1,35 @@
 package pl.zielinski.SimpleLoginAndRegisterApplication.exception;
 
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.zielinski.SimpleLoginAndRegisterApplication.domain.HttpResponse;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.time.LocalTime.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 /**
  * @author rafek
@@ -50,5 +62,90 @@ public class HandleException extends ResponseEntityExceptionHandler implements E
                 .status(HttpStatus.resolve(status.value()))
                 .statusCode(status.value())
                 .build(), status);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<HttpResponse> SQLIntegrityConstraintViolationException(SQLIntegrityConstraintViolationException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, exception.getMessage().contains("Duplicate entry") ? "Information already exists" : exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<HttpResponse> BadCredentialsException(BadCredentialsException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, exception.getMessage() + ", Incorrect email or password", exception);
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<HttpResponse> apiException(ApiException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<HttpResponse> accessDeniedException(AccessDeniedException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(HttpStatus.FORBIDDEN, "Access denied. You don\'t have access", exception);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> exception(Exception exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(INTERNAL_SERVER_ERROR, exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<HttpResponse> emptyResultDataAccessException(EmptyResultDataAccessException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> disableException(DisabledException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, "User account is currently disabled", exception);
+    }
+
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException(LockedException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, exception.getMessage() + " to many failed attempts.", exception);
+    }
+
+    @ExceptionHandler(JWTDecodeException.class)
+    public ResponseEntity<HttpResponse> exception(JWTDecodeException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(INTERNAL_SERVER_ERROR, "Could not decode the token", exception);
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<HttpResponse> dataAccessException(DataAccessException exception) {
+        log.error(exception.getMessage());
+        return createErrorHttpResponse(BAD_REQUEST, processErrorMessage(exception.getMessage()), exception);
+    }
+
+    private ResponseEntity<HttpResponse> createErrorHttpResponse(HttpStatus status, String reason, Exception exception) {
+        return new ResponseEntity<>(HttpResponse.builder()
+                .timeStamp(LocalTime.now().toString())
+                .developerMessage(exception.getMessage())
+                .reason(reason)
+                .status(status)
+                .statusCode(status.value())
+                .build(), status);
+    }
+
+    private String processErrorMessage(String errorMessage) {
+        if (errorMessage != null) {
+            if (errorMessage.contains("Duplicate entry") && errorMessage.contains("AccountVerifications")) {
+                return "You already verified your account.";
+            }
+            if (errorMessage.contains("Duplicate entry") && errorMessage.contains("ResetPasswordVerifications")) {
+                return "We already sent you an email to reset your password.";
+            }
+            if (errorMessage.contains("Duplicate entry")) {
+                return "Duplicate entry. Please try again.";
+            }
+        }
+        return "Some error occurred";
     }
 }
