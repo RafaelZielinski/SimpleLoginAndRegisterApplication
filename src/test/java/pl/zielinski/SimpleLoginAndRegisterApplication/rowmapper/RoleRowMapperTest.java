e5377baccae56e217123e5041ba5f96f13150914
rafael <rafekzielinski@wp.pl>
2024-01-23 22:07:03 +0100

    adding unit test class for RoleRowMapper

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/rowmapper/RoleRowMapperTest.java
@@ -0,0 +1,22 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.rowmapper;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleRowMapperTest {
+
+    @InjectMocks
+    RoleRowMapper roleRowMapper;
+
+    @Mock
+    private ResultSet resultSet;
+
+
+
+
+}
