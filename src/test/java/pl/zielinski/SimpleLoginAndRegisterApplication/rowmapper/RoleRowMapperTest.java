a4579782cd69953763fb70a9fe59d64f87454a63
rafael <rafekzielinski@wp.pl>
2024-01-23 22:28:07 +0100

    adding unit test for RoleRowMapper

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/rowmapper/RoleRowMapperTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/rowmapper/RoleRowMapperTest.java
@@ -1,14 +1,24 @@
 package pl.zielinski.SimpleLoginAndRegisterApplication.rowmapper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.zielinski.SimpleLoginAndRegisterApplication.domain.Role;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class RoleRowMapperTest {
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @InjectMocks
     RoleRowMapper roleRowMapper;
@@ -16,7 +26,18 @@ class RoleRowMapperTest {
     @Mock
     private ResultSet resultSet;
 
+    @Test
+    public void it_should_return_valid_map_row() throws SQLException {
+        when(resultSet.getLong("id")).thenReturn(1L);
+        when(resultSet.getString("name")).thenReturn("ROLE_USER");
+        when(resultSet.getString("permission")).thenReturn("READ:USER,READ:CUSTOMER");
 
+        Role role = roleRowMapper.mapRow(resultSet, 1);
+
+        assertEquals(role.getId(), 1L);
+        assertEquals(role.getName(), "ROLE_USER");
+        assertEquals(role.getPermission(), "READ:USER,READ:CUSTOMER");
+    }
 
 
 }
