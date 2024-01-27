ad8dd55eb5f19305c129b9d0e643ffeba7bd35a6
Rafael <rafekzielinski@wp.pl>
2024-01-27 21:03:03 +0100

    adding unit tests class for role repository

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/RoleRepositoryImplTest.java
@@ -0,0 +1,20 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.repository.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 27/01/2024
+ */
+class RoleRepositoryImplTest {
+
+
+    @Test
+    void get() {
+
+    }
+}
