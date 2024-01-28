478c1c22c3460ab72408762d41b22b4710416695
Rafael <rafekzielinski@wp.pl>
2024-01-28 21:51:39 +0100

    adding unit tests for method list in roleRepository

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/RoleRepositoryImplTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/RoleRepositoryImplTest.java
@@ -12,6 +12,10 @@ import pl.zielinski.SimpleLoginAndRegisterApplication.domain.Role;
 import pl.zielinski.SimpleLoginAndRegisterApplication.exception.ApiException;
 import pl.zielinski.SimpleLoginAndRegisterApplication.rowmapper.RoleRowMapper;
 
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -78,5 +82,56 @@ class RoleRepositoryImplTest {
         Exception actual = assertThrows(ApiException.class, () -> roleRepository.get(roleId));
         //then
         assertEquals("There is no such a role ", actual.getMessage());
+    }
+
+    //testing method list()
+    @Test
+    void it_should_return_one_role() {
+        //given
+        Role demand1 = new Role(1L, "ROLE_USER", "READ:USER,READ:CUSTOMER");
+        List<Role> expected = List.of(demand1);
+        when(jdbcTemplate.query(Mockito.anyString(), Mockito.any(RoleRowMapper.class)))
+                .thenReturn(expected);
+
+        //when
+        Collection<Role> actual = roleRepository.list();
+
+        //then
+        assertThat(actual).containsAll(expected);
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+
+    }
+
+    //testing method list()
+    @Test
+    void it_should_return_three_roles() {
+        //given
+        Role demand1 = new Role(1L, "ROLE_USER", "READ:USER,READ:CUSTOMER");
+        Role demand2 = new Role(2L, "ROLE_MANAGER", "READ:USER,READ:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER");
+        Role demand3 = new Role(3L, "ROLE_ADMIN", "READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER");
+        List<Role> expected = List.of(demand1, demand2, demand3);
+        when(jdbcTemplate.query(Mockito.anyString(), Mockito.any(RoleRowMapper.class)))
+                .thenReturn(expected);
+
+        //when
+        Collection<Role> actual = roleRepository.list();
+
+        //then
+        assertThat(actual).containsAll(expected);
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    //testing method list()
+    @Test
+    void it_should_throw_exception() {
+        //given
+        when(jdbcTemplate.query(Mockito.anyString(), Mockito.any(RoleRowMapper.class)))
+                .thenThrow(ApiException.class);
+
+        //when
+        Exception actual = assertThrows(ApiException.class, () -> roleRepository.list());
+
+        //then
+        assertEquals("An error occurred. Please try again", actual.getMessage());
     }
 }
