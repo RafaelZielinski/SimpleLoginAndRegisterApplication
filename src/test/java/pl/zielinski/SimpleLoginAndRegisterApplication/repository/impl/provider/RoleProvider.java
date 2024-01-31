2e4eded5fccc52e8df30ae7d5a0d060a3bce4162
Rafael <rafekzielinski@wp.pl>
2024-01-31 06:21:47 +0100

    improving working unit tests for roleRepositoryImpl

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/provider/RoleProvider.java
@@ -0,0 +1,23 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.repository.impl.provider;
+
+import pl.zielinski.SimpleLoginAndRegisterApplication.domain.Role;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 31/01/2024
+ */
+public interface RoleProvider {
+    default Role first() {
+        return new Role(1L, "ROLE_USER", "READ:USER,READ:CUSTOMER");
+    }
+
+    default Role second() {
+        return new Role(2L, "ROLE_MANAGER", "READ:USER,READ:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER");
+    }
+
+    default Role third() {
+        return new Role(3L, "ROLE_ADMIN", "READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER");
+    }
+}
