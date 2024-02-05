8194ceb47a8ce0bbc8e81b2caa718f44cf9d23ae
Rafael <rafekzielinski@wp.pl>
2024-02-05 22:14:39 +0100

    adding annotation autoconfiguremockmv

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/RoleControllerTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/RoleControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -31,6 +32,7 @@ import static org.mockito.Mockito.when;
  */
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(RoleController.class)
+@AutoConfigureMockMvc
 @Import(SecurityTestConfiguration.class)
 class RoleControllerTest implements RoleDTOProvider{
 
