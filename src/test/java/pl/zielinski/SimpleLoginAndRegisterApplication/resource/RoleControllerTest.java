349ea1f072184e317bfcaae1acb18787daccd606
Rafael <rafekzielinski@wp.pl>
2024-02-12 21:11:06 +0100

    I added full tests for method list()

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/RoleControllerTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/RoleControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,11 +14,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.zielinski.SimpleLoginAndRegisterApplication.exception.ApiException;
 import pl.zielinski.SimpleLoginAndRegisterApplication.provider.TokenProvider;
 import pl.zielinski.SimpleLoginAndRegisterApplication.service.RoleService;
 
+import java.util.Collections;
 import java.util.List;
 
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 /**
@@ -43,21 +48,32 @@ class RoleControllerTest implements RoleDTOProvider {
 
     @Test
     void it_should_return_list_of_two_roles() throws Exception {
-
         //given
         when(roleService.getRoles()).thenReturn(List.of(firstRoleDTO(), secondRoleDTO()));
         //when
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/roles/list")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .with(SecurityMockMvcRequestPostProcessors.user("username"))
                         .with(SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(1L, null, List.of(new SimpleGrantedAuthority("USER"))))))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("List of roles"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.roles").isArray())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.roles[0].name").value("ROLE_USER"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.roles[1].name").value("ROLE_MANAGER"));
-
     }
+
+    @Test
+    void it_should_return_empty_list_of_roles() throws Exception {
+        //given
+        given(roleService.getRoles()).willThrow(new ApiException("An error occurred. Please try again"));
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/roles/list")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(1L, null, List.of(new SimpleGrantedAuthority("USER"))))))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.developerMessage").value("An error occurred. Please try again"));
+    }
+
+
 }
