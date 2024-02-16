f1020f013f1b997863abd6c8145b0bd39e5a94fe
Rafael <rafekzielinski@wp.pl>
2024-02-16 20:08:43 +0100

    adding test class for user controller

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/UserControllerTest.java
@@ -0,0 +1,13 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 16/02/2024
+ */
+class UserControllerTest {
+
+}
