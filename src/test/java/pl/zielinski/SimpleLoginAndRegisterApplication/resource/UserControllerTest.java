e0e4acae3eb0a644c7f05972cc4a3b398be377b5
Rafael <rafekzielinski@wp.pl>
2024-02-17 22:02:46 +0100

    adding basic configuration for tests

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/UserControllerTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/UserControllerTest.java
@@ -1,6 +1,14 @@
 package pl.zielinski.SimpleLoginAndRegisterApplication.resource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.zielinski.SimpleLoginAndRegisterApplication.provider.TokenProvider;
+import pl.zielinski.SimpleLoginAndRegisterApplication.service.RoleService;
+import pl.zielinski.SimpleLoginAndRegisterApplication.service.UserService;
 
 /**
  * @author rafek
@@ -8,6 +16,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * @licence ask rafekzielinski@wp.pl
  * @since 16/02/2024
  */
-class UserControllerTest {
+@WebMvcTest
+@AutoConfigureMockMvc()
+@ActiveProfiles("test")
+class UserControllerTest implements UserDTOProvider {
+    @Autowired
+    MockMvc mockMvc;
 
+    @MockBean
+    private RoleService roleService;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private TokenProvider tokenProvider;
 }
