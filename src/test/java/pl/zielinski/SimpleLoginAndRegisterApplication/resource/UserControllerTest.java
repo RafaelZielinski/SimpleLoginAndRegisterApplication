d9dad166c63ae3cb6388bfc0f1db542765ed1abb
Rafael <rafekzielinski@wp.pl>
2024-02-27 20:52:29 +0100

    adding first test success when you register new user

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/UserControllerTest.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/resource/UserControllerTest.java
@@ -1,7 +1,9 @@
 package pl.zielinski.SimpleLoginAndRegisterApplication.resource;
 
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.zielinski.SimpleLoginAndRegisterApplication.domain.User;
 import pl.zielinski.SimpleLoginAndRegisterApplication.dto.UserDTO;
 import pl.zielinski.SimpleLoginAndRegisterApplication.exception.ApiException;
 import pl.zielinski.SimpleLoginAndRegisterApplication.provider.TokenProvider;
@@ -69,6 +72,7 @@ class UserControllerTest implements UserDTOProvider {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.user.firstName").value("Rafał"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.size()").value(1));
     }
+
     @DisplayName("Testing method getUser(Long id)")
     @Test
     void it_should_throw_exception_when_there_is_no_such_user() throws Exception {
@@ -91,8 +95,8 @@ class UserControllerTest implements UserDTOProvider {
         when(userService.getUsers()).thenReturn(List.of(first(), second()));
         //when
         mockMvc.perform(MockMvcRequestBuilders.get("/users/list")
-                .contentType(MediaType.APPLICATION_JSON)
-                .with(SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(1L, null, List.of(new SimpleGrantedAuthority("USER"))))))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(1L, null, List.of(new SimpleGrantedAuthority("USER"))))))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("List of users"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.users[0].firstName").value("Rafał"))
@@ -126,6 +130,34 @@ class UserControllerTest implements UserDTOProvider {
                         .with(SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(1L, null, List.of(new SimpleGrantedAuthority("USER"))))))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.reason").value("There is problem with list of users from database"));
+        //then
+    }
+
+    @DisplayName("Testing method register(User user)")
+    @Test
+    void it_should_register_user() throws Exception {
+        //given
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("firstName", "Rafał");
+        jsonObject.addProperty("lastName", "Zieliński");
+        jsonObject.addProperty("email", "rafekzielinski@wp.pl");
+        jsonObject.addProperty("password", "password");
+        jsonObject.addProperty("age", 44);
+
+
+        when(userService.createUser(ArgumentMatchers.any(User.class))).thenReturn(first());
+
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/register")
+                        .content(jsonObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .with(SecurityMockMvcRequestPostProcessors.authentication(
+                                new UsernamePasswordAuthenticationToken(1L, null, List.of(
+                                        new SimpleGrantedAuthority("USER"))))))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("User created"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.user.firstName").value("Rafał"));
         //then
     }
 }
