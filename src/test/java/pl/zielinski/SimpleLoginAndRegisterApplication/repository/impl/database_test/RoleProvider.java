b78b6dcbb1823a6de780964b88568ac6712ef299
Rafael <rafekzielinski@wp.pl>
2024-03-06 18:34:51 +0100

    adding sql queries in provider

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/database_test/RoleProvider.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/database_test/RoleProvider.java
@@ -7,4 +7,20 @@ package pl.zielinski.SimpleLoginAndRegisterApplication.repository.impl.database_
  * @since 06/03/2024
  */
 public interface RoleProvider {
+
+    default String deleteData() {
+        return """
+                DELETE FROM Roles;
+                """;
+    }
+
+    default String fillData() {
+        return """
+                INSERT INTO Users (
+                id, first_name, last_name, email, age, password, enabled, non_locked, using_mfa)
+                VALUES(
+                1, 'Rafał', 'Zieliński', 'rafekzielinski@wp.pl', 26,
+                '$2a$20$s521GVdPMTPsNU6tF5sISODRuJQH0rw3A5Fx8xjG8MC1QO60VNIXq', true, true, false);             
+                """;
+    }
 }
