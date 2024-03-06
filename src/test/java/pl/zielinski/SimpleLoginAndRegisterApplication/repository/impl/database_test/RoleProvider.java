900831c67cf518bf7433adebc61798150b47bf66
Rafael <rafekzielinski@wp.pl>
2024-03-06 19:02:02 +0100

    adding id to sql queries

--- src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/database_test/RoleProvider.java
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/database_test/RoleProvider.java
@@ -16,11 +16,11 @@ public interface RoleProvider {
 
     default String fillData() {
         return """
-                INSERT INTO ROLES (name, permission)
-                VALUES ('ROLE_USER', 'READ:USER,READ:CUSTOMER'),
-                ('ROLE_MANAGER', 'READ:USER,READ:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
-                ('ROLE_ADMIN', 'READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
-                ('ROLE_SYSADMIN',
+                INSERT INTO ROLES (id, name, permission)
+                VALUES (1, 'ROLE_USER', 'READ:USER,READ:CUSTOMER'),
+                (2, 'ROLE_MANAGER', 'READ:USER,READ:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
+                (3, 'ROLE_ADMIN', 'READ:USER,READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER'),
+                (4, 'ROLE_SYSADMIN',
                 'READ:USER:READ:CUSTOMER,CREATE:USER,CREATE:CUSTOMER,UPDATE:USER,UPDATE:CUSTOMER,DELETE:USER,DELETE:CUSTOMER');           
                 """;
     }
