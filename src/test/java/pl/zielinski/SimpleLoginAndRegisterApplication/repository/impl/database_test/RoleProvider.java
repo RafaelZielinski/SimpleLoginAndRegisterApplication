e28b5513b832662165af72c50806945d7755eb1d
Rafael <rafekzielinski@wp.pl>
2024-03-06 18:20:19 +0100

    adding provider for database test RoleRepository

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/database_test/RoleProvider.java
@@ -0,0 +1,10 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.repository.impl.database_test;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 06/03/2024
+ */
+public interface RoleProvider {
+}
