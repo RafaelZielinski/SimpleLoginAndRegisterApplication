19557aeae96750173a092f2dd4a4da718ab07e2e
Rafael <rafekzielinski@wp.pl>
2024-03-09 20:14:51 +0100

    adding class UserRepositoryImplTest

---
+++ src/test/java/pl/zielinski/SimpleLoginAndRegisterApplication/repository/impl/UserRepositoryImplTest.java
@@ -0,0 +1,13 @@
+package pl.zielinski.SimpleLoginAndRegisterApplication.repository.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rafek
+ * @version 1.0
+ * @licence ask rafekzielinski@wp.pl
+ * @since 09/03/2024
+ */
+class UserRepositoryImplTest {
+
+}
