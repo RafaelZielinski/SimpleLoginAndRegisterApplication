865bfa0a863561646bf3425bc435e641eb2006bc
Rafael <rafekzielinski@wp.pl>
2024-03-19 19:36:21 +0100

    adding verifications queries

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/UserQuery.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/UserQuery.java
@@ -13,4 +13,6 @@ public class UserQuery {
     public static final String UPDATE_USER_DATA_QUERY = "UPDATE Users SET first_name = :firstName, last_name = :lastName, email = :email, age = :age WHERE id = :id";
     public static final String SELECT_USERS_QUERY = "SELECT * FROM Users";
     public static final String COUNT_USER_EMAIL_QUERY = "SELECT COUNT(*) FROM Users WHERE email = :email";
+    public static final String INSERT_ACCOUNT_VERIFICATION_URL_QUERY = "INSERT INTO AccountVerifications (user_id, url) VALUES (:userId, :url)";
+
 }
