ea0f6790118a4c9556574c71bb8f230047b1c55d
Rafael <rafekzielinski@wp.pl>
2024-03-20 18:45:38 +0100

    adding queries related with accountVerifications

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/query/RoleQuery.java
@@ -12,4 +12,6 @@ public class RoleQuery {
     public static final String SELECT_ROLE_BY_USER_ID = "SELECT * FROM Roles r JOIN UserRoles ur ON ur.role_id = r.id JOIN Users u ON u.id = ur.user_id WHERE user_id = :id;";
     public static final String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO UserRoles(user_id, role_id) VALUES(:userId, :roleId);";
     public static final String SELECT_ROLE_BY_ID = "SELECT * FROM Roles WHERE id = :id;";
+    public static final String UDPATE_USER_ENABLED_QUERY = "UPDATE Users SET enabled = :enabled WHERE id = :id";
+
 }
