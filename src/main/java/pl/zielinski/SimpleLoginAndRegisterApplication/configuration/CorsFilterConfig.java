1e01ecdbec5a5a8a51de136d4cd72cc13d02c69e
Rafael <rafekzielinski@wp.pl>
2024-04-27 09:48:03 +0200

    adding update age for frontend purpose

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/configuration/CorsFilterConfig.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/configuration/CorsFilterConfig.java
@@ -18,13 +18,13 @@ import java.util.List;
 @Configuration
 public class CorsFilterConfig {
     @Bean
-    CorsFilter corsFilter() {
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList(
-                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "JwtToken", "Authorization",
+                "Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
                 "Origin", "Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
