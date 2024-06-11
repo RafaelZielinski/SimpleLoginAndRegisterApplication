82dd0d4edc6e52794b324044c945c16c5263fe37
Rafael <rafekzielinski@wp.pl>
2024-06-11 21:30:28 +0200

    changing injecting cors policy

--- src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/configuration/SecurityConfiguration.java
+++ src/main/java/pl/zielinski/SimpleLoginAndRegisterApplication/configuration/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -58,7 +59,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.ignoringRequestMatchers("/**"));
-        http.cors(AbstractHttpConfigurer::disable);
+        http.cors(Customizer.withDefaults());
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers(PUBLIC_ROUTES).permitAll()
